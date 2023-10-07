144cc5799dc4daf95ab97db548a39584cf110afa
GWYu <62191686+liudewa888@users.noreply.github.com>
2023-10-07 23:22:19 +0800

    feat: add underline style for user input (#15)

---
+++ src/components/UnderlineInput.tsx
@@ -0,0 +1,74 @@
+import "./UnderlineInput.css";
+import { ChangeEvent, useState, useEffect } from "react";
+
+export default function Question({
+  lineNum,
+  onCheckAnswer,
+}: {
+  lineNum: number;
+  onCheckAnswer: (userInput: string) => void;
+}) {
+  const [inputValue, setInputValue] = useState("");
+  const handleInput = (event: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
+
+  useEffect(() => {
+    codeItemList = document.querySelectorAll(".code-item");
+    showNum();
+    cutAct("focus");
+  }, [inputValue]);
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      onCheckAnswer(inputValue.trim());
+      setInputValue("");
+      cutAct("blur");
+    }
+  };
+
+  const showNum = () => {
+    const inputValList = inputValue.split(" ");
+    Array.from(codeItemList).map((item: any, index) => {
+      inputValList[index]
+        ? (item.innerText = inputValList[index])
+        : (item.innerText = "");
+    });
+  };
+
+  const cutAct = (type: string) => {
+    const inputValList = inputValue.split(" ");
+    const valLenth = inputValList.length;
+    Array.from(codeItemList).map((item: any) => {
+      item.className = "code-item";
+    });
+    if (type === "focus") {
+      codeItemList[valLenth - 1].className = "code-item active";
+    }
+  };
+
+  let codeItemList: any;
+  const handleInputFocus = () => {
+    codeItemList = document.querySelectorAll(".code-item");
+    cutAct("focus");
+  };
+
+  const lineInputEls = Array.from({ length: lineNum }, (_, i) => (
+    <div key={i} className="code-item"></div>
+  ));
+
+  return (
+    <div className="code-box">
+      {lineInputEls}
+      <input
+        className="code-input"
+        type="text"
+        value={inputValue}
+        onInput={handleInput}
+        onKeyDown={handleKeyDown}
+        onFocus={handleInputFocus}
+        autoFocus
+      />
+    </div>
+  );
+}
