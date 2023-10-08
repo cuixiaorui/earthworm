2abdf90862f01b3aa4d5c3f6cc81b2a6e9c4c376
Dimples_YJ <2890841438@qq.com>
2023-10-08 22:15:00 +0800

    ci: 增加代码检查工程化配置

---
+++ scripts/verifyCommit.ts
@@ -0,0 +1,28 @@
+import { readFileSync } from 'node:fs'
+import path from 'node:path'
+import chalk from 'chalk'
+
+const msgPath = path.resolve('.git/COMMIT_EDITMSG')
+const msg = readFileSync(msgPath, 'utf-8').trim()
+
+const commitRE
+  = /^(revert: )?(feat|fix|docs|dx|style|refactor|perf|test|workflow|build|ci|chore|types|wip|release)(\(.+\))?: .{1,50}/
+
+if (!commitRE.test(msg)) {
+  console.log()
+  console.error(
+    `  ${chalk.bgRed.white(' ERROR ')} ${chalk.red(
+      'invalid commit message format.',
+    )}\n\n${
+      chalk.red(
+        '  Proper commit message format is required for automated changelog generation. Examples:\n\n',
+      )
+      }    ${chalk.green('feat(compiler): add \'comments\' option')}\n`
+      + `    ${chalk.green(
+        'fix(docs): change readme (close #01)',
+      )}\n\n${
+      chalk.red('  See .github/commit-convention.md for more details.\n')}`,
+  )
+
+  process.exit(1)
+}
