3fba49b25a84ff61a92000ca5af105b1a141eed0
cuixiaorui <cui_xiaorui@126.com>
2023-10-23 17:28:33 +0800

    chore: try fix deploy again

--- src/app/course/page.tsx
+++ src/app/course/page.tsx
@@ -1,10 +1,10 @@
-import { CourseCard } from "@/components/CourseCard";
-import { fetchCourses } from "@/api/course";
+// import { CourseCard } from "@/components/CourseCard";
+// import { fetchCourses } from "@/api/course";
 
 import Link from "next/link";
 
 export default async function Course() {
-  const courses = await fetchCourses();
+  // const courses = await fetchCourses();
 
   return (
     <div className=" p-16 relative  h-screen flex flex-col ">
@@ -29,15 +29,15 @@ export default async function Course() {
         </Link>
         <h1 className="m-4 text-3xl text-indigo-500 ml-0 "> English course </h1>
       </div>
-      <div className=" overflow-y-auto scrollbar-hide h-full ">
+      <div className="overflow-y-auto scrollbar-hide h-full">
         <ul className="flex gap-14  flex-wrap p-1 overflow-y-auto md:justify-start justify-center">
-          {courses.map((course) => {
+          {/* {courses.map((course) => {
             return (
               <li key={course.id}>
                 <CourseCard course={course}></CourseCard>
               </li>
             );
-          })}
+          })} */}
         </ul>
       </div>
     </div>
