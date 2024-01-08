792953ce67a914feb27e3313b1944364c1564145
wangyi <haveaballmyball@hotmail.com>
2024-01-08 10:13:58 +0800

    fix:should be change summaryPop theme when darkMode was changed
    fix:move function to global

--- app/(main)/_components/Summary.tsx
+++ app/(main)/_components/Summary.tsx
@@ -2,10 +2,25 @@ import { fetchNextCourseId } from "@/actions/course";
 import { useCourse } from "@/store/course";
 import Link from "next/link";
 import { useEffect, useState } from "react";
+import classNames from "classnames";
 
 export function Summary() {
   const [cId, setCId] = useState(0);
+  const [isDark, setIsDark] = useState(false);
   const { currentCourse } = useCourse();
+
+  const containerClassName = classNames([
+    "h-48",
+    "bg-slate-100",
+    "shadow-lg",
+    "text-center",
+    "flex",
+    "flex-col",
+    {
+      "bg-slate-900": isDark,
+      "bg-slate0-100": !isDark,
+    },
+  ]);
 
   useEffect(() => {
     (async function () {
@@ -16,12 +31,26 @@ export function Summary() {
     })();
   });
 
+  const listener = (e: any) => {
+    setIsDark(e.value === "true");
+  };
+
+  useEffect(() => {
+    const initialIsDark = localStorage.getItem("isDarkMode") === "true";
+    setIsDark(initialIsDark);
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener("localStorageSetItem", listener);
+    return () => {
+      window.removeEventListener("localStorageSetItem", listener);
+    };
+  }, []);
+
   return (
-    <div className="h-48 bg-slate-100 shadow-lg text-center flex flex-col">
+    <div className={containerClassName}>
       <h3>总结面板</h3>
-      <div className="px-5 py-10">
-        不错不错 又学到了那么多句子和单词 加油 坚持就是胜利 :)
-      </div>
+      <div className="px-5 py-10">不错不错 又学到了那么多句子和单词 加油 坚持就是胜利 :)</div>
       <div className="w-1/3 justify-items-center bg-fuchsia-500 rounded m-auto mb-8">
         <Link href={`/?courseId=${cId}`}>开始下一课</Link>
       </div>
