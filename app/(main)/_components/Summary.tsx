8720f5127b72c5ddd626bb17b92a0db2f0f19370
hildxd <hildxd@outlook.com>
2024-01-11 10:48:44 +0800

    fix: bg设置重复

--- app/(main)/_components/Summary.tsx
+++ app/(main)/_components/Summary.tsx
@@ -17,7 +17,7 @@ export function Summary() {
   });
 
   return (
-    <div className="bg-slate-100 shadow-lg text-center bg-white flex flex-col py-8 rounded-[6px] dark:text-gray-700">
+    <div className="shadow-lg text-center bg-white flex flex-col py-8 rounded-[6px] dark:text-gray-700">
       <h3 className="font-bold">恭喜你完成了{currentCourse?.title || '本节课'}</h3>
       <div className="px-5 py-10">
         不错不错 又学到了那么多句子和单词 加油 坚持就是胜利 :)
